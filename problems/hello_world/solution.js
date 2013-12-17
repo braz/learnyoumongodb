afba06e2ed9b9ba9f6235d2b006e842ab1943862
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-17 19:12:45 +0000

    Added problems directory
    '

---
+++ problems/hello_world/solution.js
@@ -0,0 +1,10 @@
+var MongoClient = require('mongodb').MongoClient 
+  , format = require('util').format;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
+  if(err) throw err;
+
+  console.log("HELLO WORLD - WE ARE CONNECTED")
+
+  db.close();
+});
