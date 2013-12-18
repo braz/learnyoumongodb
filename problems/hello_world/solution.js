09ed02f7f5a07c779e9db2f669c2b247b09d9ccd
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-18 11:04:45 +0000

    More little fixes on solution.js P2

--- problems/hello_world/solution.js
+++ problems/hello_world/solution.js
@@ -1,5 +1,4 @@
-var MongoClient = require('mongodb').MongoClient 
-  , format = require('util').format;
+var MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
   if(err) 
