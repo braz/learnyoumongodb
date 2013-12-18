934ccf274962c17d90eee2fc3ba5c8e00263aea2
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-18 15:25:15 +0000

    Minor changes

--- problems/hello_world/setup.js
+++ problems/hello_world/setup.js
@@ -1,4 +1,3 @@
 module.exports = function () {
-  console.log("HELLO WORLD - WE ARE CONNECTED")
   return { args: [], stdin: null }
 }
--- problems/hello_world/solution.js
+++ problems/hello_world/solution.js
@@ -3,8 +3,6 @@ var MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
   if(err) 
   	throw err
-
-  console.log("HELLO WORLD - WE ARE CONNECTED")
-
-  db.close()
 })
+
+console.log("HELLO WORLD - WE ARE CONNECTED")
