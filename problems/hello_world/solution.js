3d821c626c88b70d78eb7356136e37f4adfeb193
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-18 10:59:42 +0000

    More little fixes on solution.js

--- problems/hello_world/solution.js
+++ problems/hello_world/solution.js
@@ -2,9 +2,10 @@ var MongoClient = require('mongodb').MongoClient
   , format = require('util').format;
 
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
-  if(err) throw err;
+  if(err) 
+  	throw err
 
-  console.log("HELLO WORLD - WE ARE CONNECTED");
+  console.log("HELLO WORLD - WE ARE CONNECTED")
 
-  db.close();
-});
+  db.close()
+})
