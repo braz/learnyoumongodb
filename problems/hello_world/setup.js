a8d10c577e9640f2cb219e63b6e1bd806dec4664
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-18 15:50:08 +0000

    Trouble shooting.

--- problems/hello_world/setup.js
+++ problems/hello_world/setup.js
@@ -1,3 +1,3 @@
 module.exports = function () {
-  return { args: [], stdin: null }
+  return { args: [], stdin: null, close: console.log("HELLO WORLD - WE ARE CONNECTED") }
 }
--- problems/hello_world/solution.js
+++ problems/hello_world/solution.js
@@ -1,8 +1,8 @@
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient 
+  , format = require('util').format;
 
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
-  if(err) 
-  	throw err
-})
-
-console.log("HELLO WORLD - WE ARE CONNECTED")
+  if(err) throw err;
+  
+  db.close();
+});
