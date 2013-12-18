b212605a31f120e9cf77d64964d1c0a8a36e8a0f
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-18 19:04:19 +0000

    Removed db.close() from setup.js

--- problems/my_first_query/solution.js
+++ problems/my_first_query/solution.js
@@ -2,15 +2,14 @@ var MongoClient = require('mongodb').MongoClient
   , format = require('util').format;
 
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
-  if(err) throw err;
+  if(err) console.warn(err.message);
 
     var collection = db
       .collection('test')
       .find({})
       .sort({'value': '1'})
-      .toArray(function(err, docs) {
-        console.dir(docs);
+      .toArray(function(err, object) {
+        console.dir(object);
     });
 
-    db.close();
 });
