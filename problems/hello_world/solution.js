9e8a57226d0e5380dc384d85365aa37337cc6296
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-18 10:55:46 +0000

    Added fixed missing semicolon in problem 1 solution.

--- problems/hello_world/solution.js
+++ problems/hello_world/solution.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
   if(err) throw err;
 
-  console.log("HELLO WORLD - WE ARE CONNECTED")
+  console.log("HELLO WORLD - WE ARE CONNECTED");
 
   db.close();
 });
