4d9139225f39aa84024cb8f59625f9ffe31c6c67
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-19 12:29:19 +0000

    Added refactored to set insert to write concern 1

--- problems/my_first_query/solution.js
+++ problems/my_first_query/solution.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient 
-  , format = require('util').format;
+  , format = require('util').format
 
 MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
-  if(err) console.warn(err.message);
+  if(err) console.warn(err.message)
 
     var collection = db
       .collection('test')
       .find({})
       .sort({'value': '1'})
       .toArray(function(err, object) {
-        console.dir(object);
-    });
+        console.dir(object)
+    })
 
-});
+})
