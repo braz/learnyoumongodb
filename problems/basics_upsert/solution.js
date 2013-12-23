e3f7fb4b82870c24e1b292472da1a5e6a74ad197
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-23 16:48:55 +0000

    Fixed race condition in solution.js

--- problems/basics_upsert/solution.js
+++ problems/basics_upsert/solution.js
@@ -12,17 +12,20 @@ MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db
 
     var collection = db
       .collection('test2')
-      .update(olddoc, upsertdoc, {upsert:true, multi:true, w:1}, function(err, object) {
+      .update(olddoc, {$set: upsertdoc}, {w:1, upsert:true, multi:true}, function(err, object) {
         if (err) console.warn(err.message);
-    });
 
-    db.collection('test2')
+      db.collection('test2')
       .find(doc, {fields: {_id:0}})
       .toArray(function(err, object) {
         if (err) console.warn(err.message);
         console.dir(object);
         db.close(function(err, result) {
           if (err) console.warn(err.message);
-        });
-    });
-});
+        }); // end close
+
+      }); // end find
+
+    }); // end update
+
+}); // end connect
