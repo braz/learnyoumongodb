c91762d2df491da03a31d11e34b3211c6ed2aa22
Eoin Brazil <eoin.brazil@gmail.com>
2013-12-23 12:24:53 +0000

    Edited my_first_insert exercise.

---
+++ problems/my_first_insert/solution.js
@@ -0,0 +1,31 @@
+var MongoClient = require('mongodb').MongoClient 
+  , format = require('util').format;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/learnmymongodb', function(err, db) {
+  if(err) {
+      console.warn(err.message);
+      return;
+  }
+
+    var doc = {"Learning" : "MongoDB"};
+
+    db.createCollection('test2', function(err, collection) {
+      if (err) console.warn(err.message);
+    })
+
+    var collection = db
+      .collection('test2')
+      .insert(doc, {safe: true}, function(err, object) {
+        if (err) console.warn(err.message);
+    });
+
+    db.collection('test2')
+      .find(doc)
+      .toArray(function(err, object) {
+        if (err) console.warn(err.message);
+        console.dir(object);
+        db.close(function(err, result) {
+          if (err) console.warn(err.message);
+        });
+    });
+});
