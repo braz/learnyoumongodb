8321dd0beef20c734d51109ba57125fbfb3d7ff4
Eoin Brazil <eoin.brazil@gmail.com>
2014-01-16 16:28:00 +0000

    Added basics_indexes problem.

---
+++ problems/basics_indexes/setup.js
@@ -0,0 +1,92 @@
+module.exports = function () {
+  var us = require('underscore');
+  var fs = require('fs');
+  var async = require('async');
+  var userdetailsfile = 'user2.json';
+
+  // Global definition for variables
+  var userdetailsdoc;
+  var youngest_age;
+  var oldest_age;
+  var random_age_in_range;
+  var random_age_data;
+
+  var server = 'mongodb://127.0.0.1:27017/learnmymongodb';
+  var collectionname = 'userdetails';
+  var MongoClient = require('mongodb').MongoClient 
+      , format = require('util').format;
+  //var query1age = "older_than_or_equal_to_years";
+
+  async.series([
+    // Deterime if the specified file exits
+      function(callback) {
+        fs.exists(userdetailsfile, function(exists) {
+          var err = null;
+          if (!exists) {
+            err = new Error('File does not exist \'' + userdetailsfile + '\'');
+          }
+          return callback(err);
+        });
+      },
+      // Read the file information and save to 'userdetails' JSON document and then
+      // ... get a random age from the available range in the userdetails / results
+      function(callback) {
+        var err = null;
+
+        fs.readFile(userdetailsfile, 'utf8', function(err, data) {
+        if (!data) {
+          err = new Error('Unable to read file \'' + userdetailsfile + '\'');
+        }
+        userdetailsdoc = JSON.parse(data);
+        youngest_age = us.chain(userdetailsdoc)
+          .sortBy(function(userdetailsdoc){ return userdetailsdoc.age; })
+          .map(function(userdetailsdoc){ return userdetailsdoc.age;})
+          .first()
+          .value();
+
+        oldest_age = us.chain(userdetailsdoc)
+          .sortBy(function(userdetailsdoc){ return userdetailsdoc.age; })
+          .map(function(userdetailsdoc){ return userdetailsdoc.age;})
+          .last()
+          .value();     
+
+     // random_age_in_range = us.random(parseInt(youngest_age), parseInt(oldest_age));
+     // random_age_data = { query1age : random_age_in_range };
+        return callback(err);
+        });
+      },
+      // Taking the earlier data that was stored into global variables, then add it to
+      // ... the database so we can run the exerciseß
+      function(callback) {
+        MongoClient.connect(server, function(err, db) {
+          if (err) return callback(err);
+        
+        db.createCollection(collectionname, {strict:true}, function(err, collection) {
+            //if (err) console.warn(err.message); // assume collection exists and log to console, without error it'll be created straight
+                
+              db.collection(collectionname).remove( function(err) {
+                if (err) return callback(err);
+                  
+                db.collection(collectionname).insert(userdetailsdoc, {w:1, fsync:true}, function(err, result) {
+                      if (err) return callback(err);              
+
+                      db.close(function(err, result) {
+                        if (err) return callback(err);
+                      }); //db.close      
+
+                    //db.collection(collectionname).insert(random_age_data, {w:1, fsync:true}, function(err, result) {
+                    //  if (err) return callback(err);
+                    //  }); // db.insert 2  
+                  }); // db.insert 1
+              }); //db.remove
+          }); //db.createCollection
+      }); // MongoClient.connect
+    } // callback
+  ],
+  // callback and error handling
+  function(err, results) {
+    if (err) console.warn(err.message);
+  });
+
+  return { args: [], stdin: null }
+}
---
+++ problems/basics_indexes/solution.js
@@ -0,0 +1,53 @@
+var server = 'mongodb://127.0.0.1:27017/learnmymongodb';
+var collectionname = 'userdetails';
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+var async = require('async');
+
+MongoClient.connect(server, function(err, db) {
+    if (err) {
+        console.warn(err.message);
+        return;
+    }
+
+  async.series([
+    // Deterime if the specified file exits
+      function(callback) {
+        var collection = db.collection(collectionname);
+        collection.createIndex({age:1}, {w:1}, function(err, indexName) {
+            if (err) console.warn(err.message);
+        })// createIndex 1
+         collection.createIndex({firstname:1, lastname:1}, {w:1}, function(err, indexName) {
+            if (err) console.warn(err.message);
+        })// createIndex 2
+      },
+    function(callback) {
+        var collection = db.collection(collectionname);
+        collection.find({age: { $gte: 25 }}).explain( (function(err, explaination) {
+            if (err) console.warn(err.message);
+            console.log(explaination.scanAndOrder);
+        }); // first query with explain
+      },
+    function(callback) {
+        var collection = db.collection(collectionname);
+        collection.find({age: { $lte: 35 }}).sort({firstname:1, secondname:1}).explain( (function(err, explaination) {
+            if (err) console.warn(err.message);
+            console.log(explaination.scanAndOrder);
+        }); // second query with explain
+      },
+    function(callback) {
+        var collection = db.collection(collectionname);
+        collection.find({firstname: "dominic"}).sort({firstname:1, secondname:1}).explain( (function(err, explaination) {
+            if (err) console.warn(err.message);
+            console.log(explaination.scanAndOrder);
+            db.close(function(err, result) {
+                if (err) console.warn(err.message);
+            }); // end close
+        }); // third query with explain
+      },
+  ],
+  // callback and error handling
+  function(err, results) {
+    if (err) console.warn(err.message);
+  });
+}) // connect
