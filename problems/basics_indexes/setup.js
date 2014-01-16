eb9a74332d6dfe27669fb25c7e6db70e72532bfb
Eoin Brazil <eoin.brazil@gmail.com>
2014-01-16 18:08:59 +0000

    Changed user file handling in setup.js for problems.

--- problems/basics_indexes/setup.js
+++ problems/basics_indexes/setup.js
@@ -42,8 +42,8 @@ module.exports = function () {
               }); //db.remove
           }); //db.createCollection
       }); // MongoClient.connect
+      callback(null);
     } // callback
-    callback(null);
   ],
   // callback and error handling
   function(err, results) {
