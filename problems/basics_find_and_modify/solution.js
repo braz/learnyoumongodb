aa3c191cf6772ddc28f91e916387170a73c70d60
Eoin Brazil <eoin.brazil@gmail.com>
2014-01-18 17:28:38 +0000

    Updated package and comment in solution.js for find and modify.

--- problems/basics_find_and_modify/solution.js
+++ problems/basics_find_and_modify/solution.js
@@ -5,6 +5,10 @@ var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 var async = require('async');
 var us = require('underscore');
+
+// The sleep module and call are used to delay the execution of the solution script to ensure
+// it does not check for the existing of the DB created by your program before it has a chance
+// to create the database. A lock file or other approach will be used in future.
 var sleep = require('sleep');
 sleep.sleep(1);
 
